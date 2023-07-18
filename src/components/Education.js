ad71f4692023307dbc72d53a693b9a794779111c
sporthq <nowak.sebastian991@gmail.com>
2023-07-18 23:01:21 +0200

    repair bg-color icons

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,7 @@
 import { motion, useScroll } from 'framer-motion';
 import { useRef } from 'react';
 
-const LeftBox = ({ company, date, title, author, colorIcon = "purple", childIcon = "U" }) => {
+const LeftBox = ({ company, date, title, author, colorIcon = "bg-purple-600", childIcon = "U" }) => {
 	return (
     <div
         
@@ -29,19 +29,19 @@ const LeftBox = ({ company, date, title, author, colorIcon = "purple", childIcon
 	
       <div className='xs:col-start-1 xs:col-end-2 md:col-start-5 md:col-end-6 sm:mx-0 md:mx-auto relative mr-10'>
         <div className='h-full w-6 flex items-center justify-center'></div>
-        <div className={`w-12 h-12 absolute left-0 -translate-x-[50%]  md:left-[50%] border-4 border-double  border-light top-1/2 -mt-3 rounded-full bg-${colorIcon}-600 shadw-2xl flex items-center justify-center text-light font-bold text-2xl `}>
+        <div className={`w-12 h-12 absolute left-0 -translate-x-[50%]  md:left-[50%] border-4 border-double  border-light top-1/2 -mt-3 rounded-full ${colorIcon} shadw-2xl flex items-center justify-center text-light font-bold text-2xl `}>
           {childIcon}
         </div>
       </div>
     </div>
   )
 };
-const RightBox = ({company, date, title, author, colorIcon = "purple", childIcon = "U"}) => {
+const RightBox = ({company, date, title, author, colorIcon ="bg-purple-600", childIcon = "U"}) => {
 	return (
     <div className='contents '>
 					<div className='col-start-5 col-end-6 mr-10 md:mx-auto relative'>
 						<div className='h-full w-6 flex items-center justify-center'></div>
-						<div className={`w-12 h-12 absolute left-0 -translate-x-[50%] md:left-[50%] border-4 border-double border-light top-1/2 -mt-3 rounded-full  bg-${colorIcon}-600 shadow-2xl flex items-center justify-center text-light font-bold text-2xl`}>
+						<div className={`w-12 h-12 absolute left-0 -translate-x-[50%] md:left-[50%] border-4 border-double border-light top-1/2 -mt-3 rounded-full  ${colorIcon} shadow-2xl flex items-center justify-center text-light font-bold text-2xl`}>
 							{childIcon}
 						</div>
 					</div>
@@ -94,10 +94,10 @@ const Education = () => {
        <LeftBox company='Udemy' date="2021-05-12" title="Bootstrap Od Podstaw - Stwórz 4 Wyjątkowe strony WWW!" author="Mateusz Maj"  />
 				<RightBox company='Udemy' date="2021-05-30" title="JavaScript - Jedyny Kurs, Którego Potrzebujesz!"  author="Mateusz Maj"  />
         <LeftBox company='Udemy' date="2021-05-30" title="10 projektów w czystym JavaScript" author="Mateusz Maj"   />
-				<RightBox company='teach:able' date="2021-07-30" title="Kurs Tworzenia Stron WWW cz. 3"  author="Mateusz Maj" colorIcon='green' childIcon='t'  />
-       <LeftBox company='teach:able' date="2021-09-11" title="Kurs Tworzenia Stron WWW cz. 4" author="Mateusz Maj" colorIcon='green'  childIcon='t' />
+				<RightBox company='teach:able' date="2021-07-30" title="Kurs Tworzenia Stron WWW cz. 3"  author="Mateusz Maj" colorIcon='bg-green-600' childIcon='t'  />
+       <LeftBox company='teach:able' date="2021-09-11" title="Kurs Tworzenia Stron WWW cz. 4" author="Mateusz Maj" colorIcon='bg-green-600'  childIcon='t' />
 				<RightBox company='Udemy' date="2022-11-28" title="React - The Complete Guide (incl. React Router & Redux)"  author="Maximilian Schwarzmüller"  />
-       <LeftBox company='tech:able' date="2022-01-12" title="15 zaawansowanych komponentów na strony WWW!" author="Mateusz Maj" colorIcon='green'  childIcon='t'  />
+       <LeftBox company='tech:able' date="2022-01-12" title="15 zaawansowanych komponentów na strony WWW!" author="Mateusz Maj" colorIcon='bg-green-600'  childIcon='t'  />
 				<RightBox company='Udemy' date="2022-05-23" title="The Complete JavaScript Course: From Zero to Expert!"  author="Jonas Schmedtmann"  />
        <LeftBox company='Udemy' date="2022-09-09" title="Advanced CSS and Sass: Flexbox, Grid, Animations and More!" author="Jonas Schmedtmann"  />
 				<RightBox company='Udemy' date="2022-12-28" title="Build an e-commerce app with React and Chakra UI MERN "  author="Benjamin Fischer"  />
