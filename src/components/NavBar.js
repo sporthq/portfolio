c27efed9cd673e58a91b64cf749f8a8becb18fb4
sporthq <nowak.sebastian991@gmail.com>
2023-07-20 21:57:45 +0200

    add navMobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,18 +6,41 @@ import { TwitterIcon, FaceBookIcon, GithubIcon } from './Icons';
 import { motion } from 'framer-motion';
 const CustomLink = ({ href, title, className = '' }) => {
 	const router = useRouter();
-	console.log(router);
+	
 	return (
 		<Link href={href} className={`${className} relative group`}>
 			{title}
 			<span
-				className={`h-[1px]  inline-block  bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] easy duration-300 ${
+				className={`h-[1px] inline-block  bg-dark xs:bg-light absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] easy duration-300 ${
 					router.asPath === href ? 'w-full' : 'w-0'
 				}`}
 			>
 				&nbsp;
 			</span>
 		</Link>
+	);
+};
+const CustomMobileLink = ({ href, title, className = '', toggle }) => {
+	const router = useRouter();
+
+	function handleClick(){
+		toggle()
+		router.push(href)
+	
+	}
+
+	
+	return (
+		<button onClick={handleClick} href={href} className={`${className} relative group my-2`}>
+			{title}
+			<span
+				className={`h-[1px]  inline-block  bg-light absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] easy duration-300 ${
+					router.asPath === href ? 'w-full' : 'w-0'
+				}`}
+			>
+				&nbsp;
+			</span>
+		</button>
 	);
 };
 const NavBar = () => {
@@ -31,32 +54,41 @@ const NavBar = () => {
 		<>
 			<header className='relative w-full px-32 py-6 text-xl font-medium flex  items-center justify-between  lg:pt-4 lg:justify-center '>
 				<button
-        className="absolute left-[10%] top-[50%] -translate-y-[50%] flex-col justify-center items-center hidden lg:flex "
-        onClick={handleClick}
-      >
-        <span
-          className={`bg-dark  transition-all duration-300 ease-out block  h-0.5 w-6 rounded-sm   ${
-            isOpen ? "rotate-45 translate-y-1 " : "-translate-y-0.5"
-          } `}
-        ></span>
-        <span
-          className={`bg-dark  transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5 ${
-            isOpen ? "opacity-0" : "opacity-100"
-          } `}
-        ></span>
-        <span
-          className={`bg-dark block transition-all duration-300 ease-out   h-0.5 w-6 rounded-sm   ${
-            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
-          } `}
-        ></span>
-      </button>
+					className='absolute left-[10%] top-[50%] -translate-y-[50%] flex-col justify-center items-center hidden lg:flex '
+					onClick={handleClick}
+				>
+					<span
+						className={`bg-dark  transition-all duration-300 ease-out block  h-0.5 w-6 rounded-sm   ${
+							isOpen ? 'rotate-45 translate-y-1 ' : '-translate-y-0.5'
+						} `}
+					></span>
+					<span
+						className={`bg-dark  transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5 ${
+							isOpen ? 'opacity-0' : 'opacity-100'
+						} `}
+					></span>
+					<span
+						className={`bg-dark block transition-all duration-300 ease-out   h-0.5 w-6 rounded-sm   ${
+							isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'
+						} `}
+					></span>
+				</button>
+				{isOpen && <div className='min-w-[70vw]  flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-dark/90 rounded-lg backdrop-blur-md py-32 text-light '>
+					<nav className='flex flex-col items-center jusitify-center'>
+						<CustomMobileLink href='/' title='Home' className=' 'toggle={handleClick} />
+						<CustomMobileLink href='/about' title='About' className='' toggle={handleClick}/>
+						
+						 <CustomMobileLink href='/projects' title='Projects' className=' ' toggle={handleClick}/>
+
+					</nav>
+				</div>}
 				<div className=''>
 					<Logo />
 				</div>
 				<div className='lg:hidden  '>
 					<nav className=' absolute left-[50%] translate-x-[-50%]'>
 						<CustomLink href='/' title='Home' className='mr-4 ' />
-						<CustomLink href='/about' title='About' className='mx-4' />
+						<CustomLink href='/about' title='About' className='mx-4 ' />
 						{router.asPath === '/' ? '' : <CustomLink href='/projects' title='Projects' className='mx-4' />}
 
 						{/* <CustomLink href='/contact' title='Contact' className='ml-4' /> */}
